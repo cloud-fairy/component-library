6383887d94897c42fd6d99307e7bbba3c3a6ccb7
Mor Danino <mor.danino@tikalk.com>
2023-04-09 12:43:45 +0300

    Add SG

---
+++ cloudfairy/security-group/sg.aws.tf
@@ -0,0 +1,31 @@
+variable "properties" {
+  type = any
+}
+
+variable "dependency" {
+  type = any
+}
+
+variable "project" {
+  type = any
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.17.1"
+
+  name   = var.properties.sg_name
+  vpc_id = var.dependency.network.id
+ 
+  ingress_cidr_blocks   = [var.properties.block]
+  ingress_rules         = [var.properties.rule]
+
+}
+
+output "cfout" {
+  value = {
+    network_name        = var.dependency.network.name
+    block               = var.properties.block
+    security_group_id   = module.security_group.security_group_id
+  }
+}
