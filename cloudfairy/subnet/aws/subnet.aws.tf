28df6e5d1470b9a62a121e02bd51182a1ec1a34a
Mor Danino <mor.danino@tikalk.com>
2023-05-07 11:29:54 +0300

    Add subnet validation

--- cloudfairy/subnet/aws/subnet.aws.tf
+++ cloudfairy/subnet/aws/subnet.aws.tf
@@ -1,15 +1,3 @@
-variable "properties" {
-  type = any
-}
-
-variable "dependency" {
-  type = any
-}
-
-variable "project" {
-  type = any
-}
-
 resource "aws_subnet" "main" {
   vpc_id            = var.dependency.network.id
   cidr_block        = replace(var.dependency.network.cidr, "/0\\.0/16/", var.properties.cidr)
