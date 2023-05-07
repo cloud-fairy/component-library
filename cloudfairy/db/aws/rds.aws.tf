da5ccd2fa038b09e7d9b2fe2cbc057e39dfd1d7e
Mor Danino <mor.danino@tikalk.com>
2023-05-07 17:52:04 +0300

    Add comments and align

--- cloudfairy/db/aws/rds.aws.tf
+++ cloudfairy/db/aws/rds.aws.tf
@@ -35,52 +35,52 @@ data "aws_subnets" "private" {
 }
 
 module "db" {
-  source  = "terraform-aws-modules/rds/aws"
-  version = "5.9.0"
-  create_db_instance = local.create_db
+  source                              = "terraform-aws-modules/rds/aws"
+  version                             = "5.9.0"
+  create_db_instance                  = local.create_db
 
-  identifier        = var.properties.name
+  identifier                          = var.properties.name
 
-  engine            = local.engine
-  engine_version    = local.engine_version[local.engine]
-  instance_class    = local.engine != "aurora-mysql" ? var.deafult_instance_class : var.aurora_instance_class
-  allocated_storage = var.properties.size
+  engine                              = local.engine
+  engine_version                      = local.engine_version[local.engine]
+  instance_class                      = local.engine != "aurora-mysql" ? var.deafult_instance_class : var.aurora_instance_class
+  allocated_storage                   = var.properties.size
       
-  db_name  = var.properties.name
-  username = "${var.properties.name}_admin"
-  port     = local.port
+  db_name                             = var.properties.name
+  username                            = "${var.properties.name}_admin"
+  port                                = local.port
 
   iam_database_authentication_enabled = true
 
-  vpc_security_group_ids = [var.dependency.sg.security_group_id]
+  vpc_security_group_ids              = [var.dependency.sg.security_group_id]
 
-  maintenance_window = "Mon:00:00-Mon:03:00"
-  backup_window      = "03:00-06:00"
+  maintenance_window                  = "Mon:00:00-Mon:03:00"
+  backup_window                       = "03:00-06:00"
 
   # Enhanced Monitoring - see example for details on how to create the role
   # by yourself, in case you don't want to create it automatically
-  monitoring_interval = "30"
-  monitoring_role_name = "RDSMonitoringRole-${var.properties.name}"
-  create_monitoring_role = true
+  monitoring_interval                 = "30"
+  monitoring_role_name                = "RDSMonitoringRole-${var.properties.name}"
+  create_monitoring_role              = true
 
   tags = {
-    Terraform   = "true"
-    Environment = var.project.environment_name
-    Project     = var.project.project_name
+    Terraform                         = "true"
+    Environment                       = var.project.environment_name
+    Project                           = var.project.project_name
   }
 
-  # DB subnet group
-  create_db_subnet_group = local.create_db
-  subnet_ids             = data.aws_subnets.private.ids
+  # Create DB subnet group only if DB can be created
+  create_db_subnet_group              = local.create_db
+  subnet_ids                          = data.aws_subnets.private.ids
 
   # DB parameter group
-  family = "${local.engine}${local.major_version}"
+  family                              = "${local.engine}${local.major_version}"
 
   # DB option group
-  major_engine_version =  local.major_version
+  major_engine_version                =  local.major_version
 
   # Database Deletion Protection
-  deletion_protection = false
+  deletion_protection                 = false
 }
 
 output "cfout" {
