4f5fed149d17a8b94988ec4572fdde1e36abaa9b
Mor Danino <mor.danino@tikalk.com>
2023-05-09 14:03:13 +0300

    Seperate s3 files

--- cloudfairy/storage/aws/policy.tf
+++ cloudfairy/storage/aws/policy.tf
@@ -1,39 +1,3 @@
-variable "properties" {
-  type = any
-}
-
-variable "dependency" {
-  type = any
-}
-
-variable "project" {
-  type = any
-}
-
-locals {
-  tags = {
-    Terraform       = "true"
-    Environment     = var.project.environment_name
-    Project         = var.project.project_name
-  }
-}
-
-
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-  version = "3.10.1"
-
-  bucket            = var.properties.storage_name
-  acl               = var.properties.acl != "private" ? var.properties.acl : null
-  block_public_acls = var.properties.acl == "private" ? true : false
-
-  versioning = {
-    enabled         = true
-  }
-
-  tags              = local.tags  
-}
-
 module "s3_iam_policy" {
   source            = "terraform-aws-modules/iam/aws//modules/iam-policy"
   version           = "5.18.0"
@@ -66,11 +30,6 @@ EOF
 resource "aws_iam_role_policy_attachment" "example_policy_attachment" {
   policy_arn       = module.s3_iam_policy.arn
   role             = "${var.project.project_name}_${var.project.environment_name}_admin_role"
-}
 
-output "cfout" {
-  value = {
-    storage_name  = var.properties.storage_name
-    acl           = var.properties.acl
-  }
+  depends_on       = [ module.s3_bucket ]
 }
