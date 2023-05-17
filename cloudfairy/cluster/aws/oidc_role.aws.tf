5c3b19fe5c1b1f83cd4f0bd252084a60edba4313
Mor Danino <mor.danino@tikalk.com>
2023-05-17 14:13:08 +0300

    Fix oidc cluster role

--- cloudfairy/cluster/aws/oidc_role.aws.tf
+++ cloudfairy/cluster/aws/oidc_role.aws.tf
@@ -4,8 +4,9 @@ provider "external" {
 
 # Setting a name for the Cluster irsa assuamble role
 locals {
-  role_name                     = "${var.project.project_name}_${var.project.environment_name}_${var.properties.name}"
-  policies                      = split(" ", base64decode(data.external.policies.result.ecoded_doc))
+  role_name                     = "${var.project.project_name}_${var.project.environment_name}_${var.properties.name}_irsa_role"
+  external_output               = base64decode(data.external.policies.result.ecoded_doc)
+  policies                      = local.external_output != "" ? split(" ", local.external_output) : []
   service_account               = "${var.service_account}_${var.project.environment_name}"
 }   
 
@@ -28,7 +29,7 @@ module "iam_assumable_role_admin" {
   provider_url                  = module.eks.cluster_oidc_issuer_url
 
   
-  role_policy_arns              = local.policies != [""] ? local.policies : []
+  role_policy_arns              = local.policies
 
   oidc_fully_qualified_subjects = ["system:serviceaccount:*:${var.service_account}_${var.project.environment_name}"]
 
