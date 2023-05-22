c76ffa3988189ae1061e6c6de26b118067e9a2bc
Mor Danino <mor.danino@tikalk.com>
2023-05-22 15:35:13 +0300

    Modify OIDC role

--- cloudfairy/cluster/aws/get-policies.bash
+++ cloudfairy/cluster/aws/get-policies.bash
@@ -1,9 +1,9 @@
-#!/usr/bin/env sh    
+#!/usr/bin/env bash
 
 set -euo pipefail
 
 policies=$(aws iam list-attached-role-policies --role-name $1 | jq -r '.AttachedPolicies[].PolicyArn')
 
-ecoded_doc=$(echo "$policies\c" | base64)
+ecoded_doc=$(/bin/echo -n $policies | base64)
 
 jq -n --arg ecoded_doc "$ecoded_doc" '{"ecoded_doc":$ecoded_doc}'
