aa3701c35aac77b8a93088843ade58ab06ebf80c
Mor Danino <mor.danino@tikalk.com>
2023-05-28 17:49:32 +0300

    Update service

--- cloudfairy/service/aws/env.bash
+++ cloudfairy/service/aws/env.bash
@@ -5,10 +5,16 @@
 # Change the contents of this output to get the environment variables
 # of interest. The output must be valid JSON, with strings for both
 # keys and values.
-set -euo pipefail
+set -eo pipefail
+
+if [ -z ${CI_COMMIT_SHA} ]; then
+     echo "Null!"
+     ci_commit_sha=""
+else ci_commit_sha="$CI_COMMIT_SHA"
+fi
 
 cat <<EOF
 {
-  "CI_COMMIT_SHA": "$CI_COMMIT_SHA"
+  "CI_COMMIT_SHA": "$ci_commit_sha"
 }
 EOF
