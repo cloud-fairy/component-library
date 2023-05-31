63f28c94a07539df5dab8458dfb76e7e57690209
Mor Danino <mor.danino@tikalk.com>
2023-05-31 17:40:49 +0300

    Add network private subnets tags

--- cloudfairy/network/vpc.aws.tf
+++ cloudfairy/network/vpc.aws.tf
@@ -1,68 +1,69 @@
 variable "properties" {
-  type                      = any
+  type                        = any
 }
 
 variable "dependency" {
-  type                      = any
+  type                        = any
 }
 
 variable "project" {
-  type                      = any
+  type                        = any
 }
 
 locals {
-  vpc_suffix                = "${var.dependency.cloud_provider.projectId}-${var.project.project_name}-${var.project.environment_name}"
-  vpc_prefix                = var.properties.vpc_name != "" ? var.properties.vpc_name : "vpc"
-  vpc_name                  = "${local.vpc_prefix}-${local.vpc_suffix}"
+  vpc_suffix                  = "${var.dependency.cloud_provider.projectId}-${var.project.project_name}-${var.project.environment_name}"
+  vpc_prefix                  = var.properties.vpc_name != "" ? var.properties.vpc_name : "vpc"
+  vpc_name                    = "${local.vpc_prefix}-${local.vpc_suffix}"
 }
 
 data "aws_availability_zones" "available" {}
 
 resource "aws_eip" "nat" {
-  count                     = 1
+  count                       = 1
 
-  vpc                       = true
+  vpc                         = true
 }
 
 module "vpc" {
-  source                    = "terraform-aws-modules/vpc/aws"
-  version                   = "4.0.1"
+  source                      = "terraform-aws-modules/vpc/aws"
+  version                     = "4.0.1"
 
-  name                      = local.vpc_name
+  name                        = local.vpc_name
 
-  cidr                      = var.properties.cidr_block
-  azs                       = slice(data.aws_availability_zones.available.names, 0, 2)
+  cidr                        = var.properties.cidr_block
+  azs                         = slice(data.aws_availability_zones.available.names, 0, 2)
 
-  private_subnets           = [
+  private_subnets             = [
     cidrsubnet(var.properties.cidr_block, 8, 1),
     cidrsubnet(var.properties.cidr_block, 8, 2),
   ]
-  private_subnet_tags       = {
-    type                    = "Private"
+  private_subnet_tags         = {
+    type                      = "Private"
+    "kubernetes.io/role/elb"  = "1"
   }
-  public_subnets            = var.properties.enable_public_access ? [cidrsubnet(var.properties.cidr_block, 8, 10)] : []
+  public_subnets              = var.properties.enable_public_access ? [cidrsubnet(var.properties.cidr_block, 8, 10)] : []
 
-  enable_nat_gateway        = var.properties.enable_public_access
-  single_nat_gateway        = var.properties.enable_public_access
-  one_nat_gateway_per_az    = false
-  reuse_nat_ips             = true  
-  external_nat_ip_ids       = "${aws_eip.nat.*.id}" 
-  enable_dns_hostnames      = true
+  enable_nat_gateway          = var.properties.enable_public_access
+  single_nat_gateway          = var.properties.enable_public_access
+  one_nat_gateway_per_az      = false
+  reuse_nat_ips               = true  
+  external_nat_ip_ids         = "${aws_eip.nat.*.id}" 
+  enable_dns_hostnames        = true
 
   tags = {
-    Name                    = local.vpc_name
-    Terraform               = "true"
-    Environment             = var.project.environment_name
-    Project                 = var.project.project_name
-    ProjectID               = var.dependency.cloud_provider.projectId
+    Name                      = local.vpc_name
+    Terraform                 = "true"
+    Environment               = var.project.environment_name
+    Project                   = var.project.project_name
+    ProjectID                 = var.dependency.cloud_provider.projectId
   }
 }
 
 output "cfout" {
   value = {
-    name                    = local.vpc_name
-    cidr                    = var.properties.cidr_block
-    id                      = module.vpc.vpc_id
-    private_route_table_id  = module.vpc.private_route_table_ids[0]
+    name                      = local.vpc_name
+    cidr                      = var.properties.cidr_block
+    id                        = module.vpc.vpc_id
+    private_route_table_id    = module.vpc.private_route_table_ids[0]
   }
 }
