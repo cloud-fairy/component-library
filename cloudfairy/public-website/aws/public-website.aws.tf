d493bb5d1beca22a036bf5b1848acb7a7d69664d
Mor Danino <mor.danino@tikalk.com>
2023-06-04 13:45:11 +0300

    Fix public website

--- cloudfairy/public-website/aws/public-website.aws.tf
+++ cloudfairy/public-website/aws/public-website.aws.tf
@@ -1,60 +1,82 @@
 variable "properties" {
-  type              = any
+  type                    = any
 }
 
 variable "dependency" {
-  type              = any
+  type                    = any
 }
 
 variable "project" {
-  type              = any
+  type                    = any
 }
 
 locals {
-  bucketName        = "${var.properties.bucketName}-${local.tags.Project}-${local.tags.Environment}"
+  bucketName              = "${var.properties.bucketName}-${local.tags.Project}-${local.tags.Environment}"
   
-  tags = {
-    Terraform       = "true"
-    Environment     = var.project.environment_name
-    Project         = var.project.project_name
-    ProjectID       = var.dependency.cloud_provider.projectId
+  tags                    = {
+    Terraform             = "true"
+    Environment           = var.project.environment_name
+    Project               = var.project.project_name
+    ProjectID             = var.dependency.cloud_provider.projectId
   }
 }
 
 
 module "s3_bucket" {
-  source            = "terraform-aws-modules/s3-bucket/aws"
-  version           = "3.10.1"
+  source                  = "terraform-aws-modules/s3-bucket/aws"
+  version                 = "3.10.1"
 
-  bucket            = local.bucketName
-  acl               = "public-read"
+  bucket                  = local.bucketName
+  block_public_acls       = false
+  restrict_public_buckets = false
+  block_public_policy     = false
+  attach_public_policy    = true
 
   website = {
 
-    index_document  = var.properties.indexPage
-    error_document  = var.properties.errorPage
-    routing_rules = [{
-      condition = {
-        key_prefix_equals = "docs/"
-      },
-      redirect = {
-        replace_key_prefix_with = "documents/"
-      }
-    }]
+    index_document        = var.properties.indexPage
+    error_document        = var.properties.errorPage
+    # routing_rules   = [{
+    #   condition = {
+    #     key_prefix_equals = "docs/"
+    #   },
+    #   redirect = {
+    #     replace_key_prefix_with = "documents/"
+    #   }
+    # }]
+  }
+  attach_policy           = true
+  policy                  =     <<EOF
+  {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::${local.bucketName}/*"
+            ]
+        }
+    ]
+}
+  EOF
+
+  versioning              = {
+    enabled               = true
   }
 
-  versioning        = {
-    enabled         = true
-  }
-
-  tags              = local.tags  
+  tags                    = local.tags  
 }
 
 
 output "cfout" {
-  value             = {
-    storage_name    = local.bucketName
-    policy_arn      = module.bucket_iam_policy.arn
-    instructions    = "deployment: aws s3 sync <Source Folder> s3://${local.bucketName}/path-to-folder/"
+  value                   = {
+    storage_name          = local.bucketName
+    policy_arn            = module.bucket_iam_policy.arn
+    instructions          = "deployment: aws s3 sync <Source Folder> s3://${local.bucketName}/path-to-folder/"
   }
 }
