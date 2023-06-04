d37b242fc553809b0ba79f70b34370770153363c
Mor Danino <mor.danino@tikalk.com>
2023-06-04 16:17:43 +0300

    Add dockerhub component

---
+++ cloudfairy/docker/aws/docker.aws.tf
@@ -0,0 +1,163 @@
+variable "properties" {
+  # service_name
+  # repo_url
+  type                 = any
+}
+
+variable "project" {
+  # environment_name
+  type                 = any
+}
+
+variable "dependency" {
+  # cloud_provider
+  # cluster
+  type                 = any
+}
+
+variable "connector" {
+  # cloudfairy_k8_microservice_to_managed_sql : any[]
+  type                 = any
+}
+
+terraform {
+  required_providers {
+    external           = {
+      source           = "hashicorp/external"
+      version          = "2.3.1"
+    }
+  }
+}
+
+locals {
+
+  tags                 = {
+    Terraform          = "true"
+    Environment        = var.project.environment_name
+    Project            = var.project.project_name
+    ProjectID          = var.dependency.cloud_provider.projectId
+  }
+
+  service_name         = var.properties.service_name
+  dockerhub_image      = var.properties.dockerhub_image
+  conn_to_services     = try(var.connector.cloudfairy_service_to_service, [])
+  inject_env_vars      = flatten(local.conn_to_services)
+}
+
+
+
+resource "local_file" "deployment" {
+  filename             = "${path.module}/../../../../../../../.cloudfairy/ci-cd/${local.service_name}.deployment.yaml"
+  content              = <<EOF
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: ${var.dependency.cluster.service_account}
+  namespace: default
+  annotations:
+    eks.amazonaws.com/role-arn: ${var.dependency.cluster.irsa_role_arn}
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: ${local.service_name}
+  namespace: default
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: ${local.service_name}
+  template:
+    metadata:
+      labels:
+        app: ${local.service_name}
+    spec:
+      serviceAccountName: ${var.dependency.cluster.service_account}
+      containers:
+        - name: ${local.service_name}
+          image: ${local.dockerhub_image}
+          ${length(local.inject_env_vars) > 0 ? "env:\n            ${indent(12, yamlencode(local.inject_env_vars))}" : "env: []" }
+          imagePullPolicy: Always
+          ports:
+            - containerPort: ${local.container_port}
+              protocol: TCP
+          resources:
+            limits:
+              memory: "1Gi"
+              cpu: "500m"
+EOF
+}
+
+resource "local_file" "service" {
+  filename             = "${path.module}/../../../../../../../.cloudfairy/ci-cd/${local.service_name}.service.yaml"
+  content              = <<EOF
+apiVersion: v1
+kind: Service
+metadata:
+  name: ${local.service_name}
+  namespace: default
+  labels:
+    app: ${local.service_name}
+spec:
+  type: ClusterIP
+  ports:
+    - port: ${local.container_port}
+      targetPort: ${local.container_port}
+      protocol: TCP
+  selector:
+    app: ${local.service_name}
+EOF
+}
+
+resource "local_file" "ingress" {
+  count         = var.properties.isexposed ? 1 : 0
+  
+  filename      = "${path.module}/../../../../../../../.cloudfairy/ci-cd/${local.service_name}.ingress.yaml"
+  content       = <<EOF
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: ${local.service_name}
+  namespace: default
+  labels:
+    app: ${local.service_name}
+  annotations:
+    alb.ingress.kubernetes.io/listen-ports: '[{"HTTP": 80}]'
+    alb.ingress.kubernetes.io/scheme: internet-facing
+    alb.ingress.kubernetes.io/target-type: ip
+    external-dns.alpha.kubernetes.io/hostname: ${local.service_name}.${local.tags.Project}.tikalk.dev
+    alb.ingress.kubernetes.io/inbound-cidrs: "0.0.0.0/0, ::/0"
+spec:
+  ingressClassName: alb
+  rules:
+    - host: ${local.service_name}.${local.tags.Project}.tikalk.dev
+      http:
+        paths:
+          - path: /
+            pathType: Prefix
+            backend:
+              service:
+                name: ${local.service_name}
+                port:
+                  number: ${local.container_port}
+  EOF
+}
+
+resource "local_file" "lifecycle" {
+  filename             = "${path.module}/../../../../../../../.cloudfairy/ci-cd/${local.service_name}.cloudfairy-lifecycle.sh"
+  content              = <<EOF
+#!/usr/bin/env sh
+
+set -x
+find . -type f -name '${local.service_name}.docker-build.ci.sh' -exec {} ';'
+find . -type f -name '${local.service_name}.*.yaml' -exec kubectl apply -f {} ';'
+  EOF
+}
+
+output "cfout" {
+  value = {
+    service_hostname   = local.service_name
+    service_port       = 80
+    env_vars           = local.inject_env_vars
+  }
+}
