42d3b75c8cd4071b45c2fef2c5e3da7412cbe09c
Mor Danino <mor.danino@tikalk.com>
2023-06-05 19:30:57 +0300

    Fix certificate regex

--- cloudfairy/certificate/cert.aws.tf
+++ cloudfairy/certificate/cert.aws.tf
@@ -1,13 +1,13 @@
 variable "properties" {
-  type = any
+  type                      = any
 }
 
 variable "dependency" {
-  type = any
+  type                      = any
 }
 
 variable "project" {
-  type = any
+  type                      = any
 }
 
 locals {
@@ -15,47 +15,47 @@ locals {
   use_existing_route53_zone = true
 
   # Removing trailing dot from domain - just to be sure :)
-  domain_name = trim(regex("\\..*$", var.properties.hostname), ".")
+  domain_name               = trim(regex("[\\w]*\\.[\\w]*$", var.properties.hostname), ".")
 
-  zone_id = try(data.aws_route53_zone.this[0].zone_id, aws_route53_zone.this[0].zone_id, null)
+  zone_id                   = try(data.aws_route53_zone.this[0].zone_id, aws_route53_zone.this[0].zone_id, null)
 }
 
 data "aws_route53_zone" "this" {
-  count = local.use_existing_route53_zone ? 1 : 0
+  count                     = local.use_existing_route53_zone ? 1 : 0
 
-  name         = local.domain_name
-  private_zone = false
+  name                      = local.domain_name
+  private_zone              = false
 }
 
 resource "aws_route53_zone" "this" {
-  count = !local.use_existing_route53_zone ? 1 : 0
+  count                     = !local.use_existing_route53_zone ? 1 : 0
 
-  name = local.domain_name
+  name                      = local.domain_name
 }
 
 module "acm" {
-  count = local.zone_id != null ? 1 : 0
+  count                     = local.zone_id != null ? 1 : 0
 
-  source  = "terraform-aws-modules/acm/aws"
-  version = "4.3.2"
+  source                    = "terraform-aws-modules/acm/aws"
+  version                   = "4.3.2"
 
-  domain_name = local.domain_name
-  zone_id     = local.zone_id
+  domain_name               = local.domain_name
+  zone_id                   = local.zone_id
 
   subject_alternative_names = [
     "${var.properties.hostname}",
   ]
 
   tags = {
-    Name = local.domain_name
+    Name                    = local.domain_name
   }
 }
 
 output "cfout" {
-  value = {
-    name    =  var.properties.hostname
-    domain  =  local.domain_name
-    arn     =  module.acm[0].acm_certificate_arn
-    status  =  module.acm[0].acm_certificate_status
+  value                     = {
+    name                    =  var.properties.hostname
+    domain                  =  local.domain_name
+    arn                     =  module.acm[0].acm_certificate_arn
+    status                  =  module.acm[0].acm_certificate_status
   }
 }
