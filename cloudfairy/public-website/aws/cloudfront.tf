1bb87ba8a1e83501e8360707485a456168fc25ce
Mor Danino <mor.danino@tikalk.com>
2023-06-11 11:57:04 +0300

    Add Cloudfront

--- cloudfairy/public-website/aws/cloudfront.tf
+++ cloudfairy/public-website/aws/cloudfront.tf
@@ -1,5 +1,37 @@
 locals {
-  s3_origin_id                      = "myS3Origin-${local.bucketName}"
+  s3_origin_id                      = local.bucketName
+}
+
+###################################
+# IAM Policy Document
+###################################
+data "aws_iam_policy_document" "read_bucket" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${module.s3_bucket.s3_bucket_arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.bucket.iam_arn]
+    }
+  }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = [module.s3_bucket.s3_bucket_arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.bucket.iam_arn]
+    }
+  }
+}
+###################################
+# S3 Bucket Policy
+###################################
+resource "aws_s3_bucket_policy" "read_gitbook" {
+  bucket                            = module.s3_bucket.s3_bucket_id
+  policy                            = data.aws_iam_policy_document.read_bucket.json
 }
 
 resource "aws_cloudfront_origin_access_control" "bucket" {
@@ -10,11 +42,19 @@ resource "aws_cloudfront_origin_access_control" "bucket" {
   signing_protocol                  = "sigv4"
 }
 
+resource "aws_cloudfront_origin_access_identity" "bucket" {
+  comment                           = local.bucketName
+}
+
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name                     = module.s3_bucket.s3_bucket_bucket_regional_domain_name
-    origin_access_control_id        = aws_cloudfront_origin_access_control.bucket.id
+    #origin_access_control_id        = aws_cloudfront_origin_access_control.bucket.id
     origin_id                       = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity        = aws_cloudfront_origin_access_identity.bucket.cloudfront_access_identity_path
+    }
   }
 
   enabled                           = true
@@ -27,7 +67,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   default_cache_behavior {
     allowed_methods                 = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
     cached_methods                  = ["GET", "HEAD"]
-    target_origin_id                = local.s3_origin_id
+    target_origin_id                = local.bucketName
 
     forwarded_values {
       query_string                  = false
@@ -37,61 +77,16 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       }
     }
 
-    viewer_protocol_policy          = "allow-all"
+    viewer_protocol_policy          = "redirect-to-https"
     min_ttl                         = 0
     default_ttl                     = 3600
     max_ttl                         = 86400
   }
 
-  # Cache behavior with precedence 0
-  ordered_cache_behavior {
-    path_pattern     = "/content/immutable/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD", "OPTIONS"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string = false
-      headers      = ["Origin"]
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    min_ttl                = 0
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    compress               = true
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  # Cache behavior with precedence 1
-  ordered_cache_behavior {
-    path_pattern     = "/content/*"
-    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = local.s3_origin_id
-
-    forwarded_values {
-      query_string                 = false
-
-      cookies {
-        forward                    = "none"
-      }
-    }
-
-    min_ttl                        = 0
-    default_ttl                    = 3600
-    max_ttl                        = 86400
-    compress                       = true
-    viewer_protocol_policy         = "redirect-to-https"
-  }
-
   restrictions {
     geo_restriction {
-      restriction_type             = "blacklist"
-      locations                    = ["CA"]
+      restriction_type = "blacklist"
+      locations        = ["DE"]
     }
   }
 
@@ -103,5 +98,6 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     cloudfront_default_certificate = false
     acm_certificate_arn            = var.dependency.certificate.arn
     ssl_support_method             = "sni-only"
+    #minimum_protocol_version = "TLSv1.2_2018"
   }
 }
