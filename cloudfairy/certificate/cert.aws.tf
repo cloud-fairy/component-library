54117b03492f5a8bb975275a59fc7517f4220cd1
Mor Danino <mor.danino@tikalk.com>
2023-06-12 14:56:47 +0300

    update domain_name

--- cloudfairy/certificate/cert.aws.tf
+++ cloudfairy/certificate/cert.aws.tf
@@ -17,7 +17,7 @@ variable "project" {
 
 locals {
   # Removing trailing dot from domain - just to be sure :)
-  domain_name               = trim(regex("[\\w]+\\.[\\w]+\\.[\\w]+$", local.hostname), ".")
+  domain_name               = lower("${local.tags.Project}.tikalk.dev")
   zone_name                 = lower("${local.tags.Project}.tikalk.dev")
   hostname                  = lower("*.${local.tags.Project}.tikalk.dev")
   project                   = var.project.project_name
