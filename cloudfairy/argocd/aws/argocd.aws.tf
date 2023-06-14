2d7a5c50d56870fb3f056b5a2a3b6ae0990720a7
Mor Danino <mor.danino@tikalk.com>
2023-06-14 16:02:39 +0300

    Update components load-balancer name

--- cloudfairy/argocd/aws/argocd.aws.tf
+++ cloudfairy/argocd/aws/argocd.aws.tf
@@ -89,7 +89,7 @@ locals {
           kubernetes.io/ingress.class: alb
           alb.ingress.kubernetes.io/target-type: ip
           alb.ingress.kubernetes.io/scheme: internet-facing
-          alb.ingress.kubernetes.io/load-balancer-name: "argocd-${local.tags.Project}-${local.tags.Environment}"
+          alb.ingress.kubernetes.io/load-balancer-name: ${lower("${local.tags.Project}-${local.tags.Environment}")}
           alb.ingress.kubernetes.io/group.name: ${lower("${local.tags.Project}-${local.tags.Environment}")}
           alb.ingress.kubernetes.io/listen-ports: '[{"HTTP": 80},{"HTTPS": 443}]'
           alb.ingress.kubernetes.io/certificate-arn: "${var.dependency.certificate.arn}"
